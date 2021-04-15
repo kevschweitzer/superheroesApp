a4f84ae179e4bcb30bc69727e246f5b9e17375b9
Kevin Schweitzer <kschweitzer@mobilecomputing.com.ar>
2021-04-15 16:56:55 -0300

    Refactor superheroesService injection

--- app/src/main/java/com/schweitzering/superheroesapp/di/AppModules.kt
+++ app/src/main/java/com/schweitzering/superheroesapp/di/AppModules.kt
@@ -1,14 +1,28 @@
 package com.schweitzering.superheroesapp.di
 
 import androidx.fragment.app.FragmentManager
+import com.facebook.stetho.okhttp3.StethoInterceptor
 import com.schweitzering.superheroesapp.NavigationManager
+import com.schweitzering.superheroesapp.data.SuperHeroesService
 import com.schweitzering.superheroesapp.data.SuperheroesRepositoryImpl
 import com.schweitzering.superheroesapp.domain.GetAllSuperHeroesUseCase
 import com.schweitzering.superheroesapp.domain.SuperheroesRepository
 import com.schweitzering.superheroesapp.presentation.SuperheroesViewModel
+import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val client = OkHttpClient.Builder()
+        .addNetworkInterceptor(StethoInterceptor()).build()
+
+val retrofit = Retrofit.Builder()
+        .client(client)
+        .baseUrl("https://gateway.marvel.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
 val appModule = module {
 
@@ -25,7 +39,9 @@ val appModule = module {
     }
 
     factory<SuperheroesRepository> {
-        SuperheroesRepositoryImpl()
+        SuperheroesRepositoryImpl(
+            retrofit.create(SuperHeroesService::class.java)
+        )
     }
 
     single {
